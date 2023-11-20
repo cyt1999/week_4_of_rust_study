acd108a9d2135f1855d96c4e39484856a823d878
chenyutong <xb990815@gmail.com>
2023-11-20 14:42:37 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,55 @@
 // 使用枚举
-struct TypeA {
-    data: String,
-}
+// struct TypeA {
+//     data: String,
+// }
 
-struct TypeB {
-    data: i32,
-}
+// struct TypeB {
+//     data: i32,
+// }
 
-struct TypeC {
-    data: f64,
-}
+// struct TypeC {
+//     data: f64,
+// }
 
-enum MyEnum {
-    A(TypeA),
-    B(TypeB),
-    C(TypeC),
-}
+// enum MyEnum {
+//     A(TypeA),
+//     B(TypeB),
+//     C(TypeC),
+// }
 
-impl TypeA {
-    fn do_something(&self) {
-        println!("TypeA: {}", self.data);
-    }
-}
+// impl TypeA {
+//     fn do_something(&self) {
+//         println!("TypeA: {}", self.data);
+//     }
+// }
 
-impl TypeB {
-    fn do_something(&self) {
-        println!("TypeB: {}", self.data);
-    }
-}
+// impl TypeB {
+//     fn do_something(&self) {
+//         println!("TypeB: {}", self.data);
+//     }
+// }
 
-impl TypeC {
-    fn do_something(&self) {
-        println!("TypeC: {}", self.data);
-    }
-}
+// impl TypeC {
+//     fn do_something(&self) {
+//         println!("TypeC: {}", self.data);
+//     }
+// }
 
-fn main() {
-    let my_vec: Vec<MyEnum> = vec![
-        MyEnum::A(TypeA { data: "Hello".to_string() }),
-        MyEnum::B(TypeB { data: 42 }),
-        MyEnum::C(TypeC { data: 3.14 }),
-    ];
+// fn main() {
+//     let my_vec: Vec<MyEnum> = vec![
+//         MyEnum::A(TypeA { data: "Hello".to_string() }),
+//         MyEnum::B(TypeB { data: 42 }),
+//         MyEnum::C(TypeC { data: 3.14 }),
+//     ];
 
-    for item in my_vec {
-        match item {
-            MyEnum::A(a) => a.do_something(),
-            MyEnum::B(b) => b.do_something(),
-            MyEnum::C(c) => c.do_something(),
-        }
-    }
-}
+//     for item in my_vec {
+//         match item {
+//             MyEnum::A(a) => a.do_something(),
+//             MyEnum::B(b) => b.do_something(),
+//             MyEnum::C(c) => c.do_something(),
+//         }
+//     }
+// }
 // TypeA: Hello
 // TypeB: 42
 // TypeC: 3.14
@@ -105,3 +105,46 @@ fn main() {
 // TypeA: Hello
 // TypeB: 42
 // TypeC: 3.14
+
+
+
+use std::ops::Add;
+
+#[derive(Debug)]
+struct MyType {
+    value: i32,
+}
+
+impl Add for MyType {
+    type Output = MyType;
+
+    fn add(self, other: MyType) -> MyType {
+        MyType {
+            value: self.value + other.value,
+        }
+    }
+}
+
+
+trait MyTrait {
+    fn do_something(&self);
+}
+
+impl MyTrait for MyType {
+    fn do_something(&self) {
+        println!("Doing something with MyType: {:?}", self);
+    }
+}
+
+fn use_trait_object(obj: &dyn MyTrait) {
+    obj.do_something();
+}
+
+
+fn main() {
+    let a = MyType { value: 10 };
+    let b = MyType { value: 20 };
+    let result = a + b;
+
+    use_trait_object(&result);
+}
